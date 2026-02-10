cb3e495ac1560077a58afc2630eb0d17b571bade
Daisuke Nishimori <daisukn@outlook.com>
2026-02-10 20:57:59 +0900

    A

--- ToyLib/src/Engine/Core/Application.cpp
+++ ToyLib/src/Engine/Core/Application.cpp
@@ -120,32 +120,6 @@ bool Application::Initialize()
     SDL_SetWindowPosition(mWindow, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
 
-    //---------------------------------------------------------
-    // OpenGL コンテキスト属性設定
-    //---------------------------------------------------------
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    
-    //---------------------------------------------------------
-    // OpenGL コンテキスト生成
-    //---------------------------------------------------------
-    mGLContext = SDL_GL_CreateContext(mWindow);
-    if (!mGLContext)
-    {
-        std::cerr << "Failed to create GL context: " << SDL_GetError() << std::endl;
-        return false;
-    }
-
     // Renderer 初期化（）
     if (!mRenderer->Initialize(this))
     {
--- ToyLib/src/Render/GL/GLRenderer_Core.cpp
+++ ToyLib/src/Render/GL/GLRenderer_Core.cpp
@@ -25,6 +25,34 @@ bool GLRenderer::Initialize(const Application* app)
     mWindow    = app->GetSDLWindow();      // 非所有
     mGLContext = app->GetGLContext();   // 非所有
 
+    //---------------------------------------------------------
+    // OpenGL コンテキスト属性設定
+    //---------------------------------------------------------
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    
+    //---------------------------------------------------------
+    // OpenGL コンテキスト生成
+    //---------------------------------------------------------
+    mGLContext = SDL_GL_CreateContext(mWindow);
+    if (!mGLContext)
+    {
+        std::cerr << "Failed to create GL context: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    
+    
     // VSync
     SDL_GL_SetSwapInterval(1);
 
