ca41809f934089fba24b167f2c001f2f9ac891ae
Daisuke Nishimori <daisukn@outlook.com>
2026-02-15 04:24:45 +0900

    A

--- ToyGame/TestApp/TestApp.cpp
+++ ToyGame/TestApp/TestApp.cpp
@@ -22,7 +22,8 @@ void VKTest::InitGame()
     
     auto islandActor = CreateActor<toy::Actor>();
     auto islandMesh = islandActor->CreateComponent<toy::MeshComponent>();
-    islandMesh->SetMesh(GetAssetManager()->GetMesh("island.x"));
+    islandMesh->SetMesh(GetAssetManager()->GetMesh("brick.x"));
+    islandActor->SetPosition(Vector3(0.0, 0.0f, 10.0f));
     
 }
 
