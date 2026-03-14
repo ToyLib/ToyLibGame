c53bcec3b543cb3ac751f0f69917bb9368baca92
Daisuke Nishimori <daisukn@outlook.com>
2026-03-15 01:41:57 +0900

    A

--- ToyGame/KitGame/Scenes/FieldScene.cpp
+++ ToyGame/KitGame/Scenes/FieldScene.cpp
@@ -187,7 +187,7 @@ void FieldScene::DeploySky()
     mWeather = std::make_unique<toy::WeatherManager>();
     mWeather->SetWeatherDome(dome);
     mWeather->SetWeatherOverlay(overlay);
-    mWeather->ChangeWeather(toy::WeatherType::SNOW);
+    mWeather->ChangeWeather(toy::WeatherType::CLEAR);
 }
 
 
