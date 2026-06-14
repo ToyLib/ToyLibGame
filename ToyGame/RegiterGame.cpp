336984dddc21becaec2244098947c07c949c2c0b
Daisuke Nishimori <daisukn@outlook.com>
2026-06-15 01:09:00 +0900

    A

--- ToyGame/RegiterGame.cpp
+++ ToyGame/RegiterGame.cpp
@@ -4,7 +4,7 @@
 #include "TestApp/TestApp.h"
 
 // ToyLibの起動Applicationとして登録
-//TOYLIB_REGISTER_APP(GameRPG)
-TOYLIB_REGISTER_APP(GameApp)
+TOYLIB_REGISTER_APP(GameRPG)
+//TOYLIB_REGISTER_APP(GameApp)
 //TOYLIB_REGISTER_APP(VKTest)
 
