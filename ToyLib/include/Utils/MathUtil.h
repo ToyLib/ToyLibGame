154e2ae8421f2f6428c86852ca866db7775bde99
Daisuke Nishimori <daisukn@outlook.com>
2026-08-21 00:31:42 +0900

    operator追加

--- ToyLib/include/Utils/MathUtil.h
+++ ToyLib/include/Utils/MathUtil.h
@@ -335,6 +335,15 @@ public:
         return *this;
     }
 
+    friend bool operator==(const Vector3& a, const Vector3& b)
+    {
+        return a.x == b.x && a.y == b.y && a.z == b.z;
+    }
+    friend bool operator!=(const Vector3& a, const Vector3& b)
+    {
+        return !(a == b);
+    }
+
     float LengthSq() const
     {
         return x * x + y * y + z * z;
